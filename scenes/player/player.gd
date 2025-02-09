ac58a57327f1ee3a8a48a425ba1098c58db713d2
AyaPK <Sherbnya@gmail.com>
2025-02-09 16:35:14 +0000

    Turn when moving

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 var speed: int = 75
 var direction: Vector2 = Vector2.DOWN
+@onready var player: Sprite2D = $Player
 
 func _physics_process(delta: float) -> void:
 	var input_dir: Vector2 = Vector2(
@@ -9,11 +10,19 @@ func _physics_process(delta: float) -> void:
 		).normalized()
 	if input_dir.x > 0:
 		direction = input_dir
+		player.frame = 1
+		player.flip_h = false
 	elif input_dir.x < 0:
 		direction = input_dir
+		player.frame = 1
+		player.flip_h = true
 	elif input_dir.y < 0:
 		direction = input_dir
+		player.frame = 2
+		player.flip_h = false
 	elif input_dir.y > 0:
 		direction = input_dir
+		player.frame = 0
+		player.flip_h = false
 	velocity = input_dir*speed
 	move_and_slide()
